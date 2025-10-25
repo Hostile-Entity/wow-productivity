ea1d7463061c6598002848e02aeceafbc35db7e4
User1 <jackal2898@gmail.com>
2025-10-25 00:13:50 -0400

    fix for path

--- src/sw-register.ts
+++ src/sw-register.ts
@@ -1,7 +1,8 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
+    const swUrl = `${(import.meta as any).env.BASE_URL}sw.js`;
     navigator.serviceWorker
-      .register('/sw.js')
+      .register(swUrl)
       .catch((e) => console.warn('SW registration failed:', e));
   });
 }
